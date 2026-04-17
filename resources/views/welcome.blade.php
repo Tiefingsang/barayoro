e800526cbc02ef39b2e3d2b1d850ced9c437806f
Tiefing Sangaré <tiefingsangare86@gmail.com>
2026-04-17 11:45:34 +0000

    merge home_page at Myra

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,7 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
+    
     <style>
         * {
             font-family: 'Inter', sans-serif;
